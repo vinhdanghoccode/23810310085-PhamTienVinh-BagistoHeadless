b825266ee2e48423bdbd5495f0696528ee3ca16d
vinhdanghoccode <phamtienvinh5002@gmail.com>
2026-04-03 16:38:46 +0700

    change readme, add image

--- bagisto-headless/config/cors.php
+++ bagisto-headless/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'graphql', 'graphiql', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
